c2f4764e45979b6727fae35bdaa3ff3a42c5b317
dan-at-mimas <daniel.needham@manchester.ac.uk>
2016-10-21 20:38:26 +0100

    added publication place aggregator

---
+++ tools/publicationplaceaggregator.py
@@ -0,0 +1,25 @@
+from fieldaggregator import FieldAggregator
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+class PublicationPlaceAggregator(FieldAggregator):
+
+	def normalise_value(self, value):
+		return value.replace(" ","")
+
+publication_place_aggregator = PublicationPlaceAggregator()
+aggregated_fields = publication_place_aggregator.aggregate_field("/Users/danielneedham/tmp/sample/anatomy/publications.csv","publication places")
+
+labels = aggregated_fields.keys()
+values = aggregated_fields.values()
+
+fig = {
+    'data': [{'labels': labels,
+              'values': values,
+              'type': 'pie'}],
+    'layout': {'title': 'Publication places'}
+     }
+
+
+py.plot(fig)
+		
